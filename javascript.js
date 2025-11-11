3276e7582225621467aacd4bc5aa0dfac608769f
gbntb <228612152+gbntb@users.noreply.github.com>
2025-11-11 14:06:56 -0400

    Add getComputerChoice() function

---
+++ javascript.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+    }
+}
