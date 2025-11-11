13b6e0c48da312942aebb31544f0c4dda4070288
gbntb <228612152+gbntb@users.noreply.github.com>
2025-11-11 14:43:27 -0400

    Create playRound() function with the game's logic

--- javascript.js
+++ javascript.js
@@ -22,3 +22,46 @@ function getHumanChoice() {
 
     return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("It's a tie! You both played rock!");
+                break;
+            case "paper":
+                console.log("You lose! Paper beats rock!");
+                computerScore++;
+                break;
+            case "scissors":
+                console.log("You win! Rock beats scissors!");
+                humanScore++;
+        }
+    } else if (humanChoice === "paper") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("You win! Paper beats rock!");
+                humanScore++;
+                break;
+            case "paper":
+                console.log("It's a tie! You both played paper!");
+                break;
+            case "scissors":
+                console.log("You lose! Scissors beats paper!");
+                computerScore++;
+        }
+    } else if (humanChoice === "scissors") {
+        switch (computerChoice) {
+            case "rock":
+                console.log("You lose! Rock beats scissors!");
+                computerScore++;
+                break;
+            case "paper":
+                console.log("You win! Scissors beats paper!");
+                humanScore++;
+                break;
+            case "scissors":
+                console.log("It's a tie! You both played scissors!");
+        }
+    }
+}
